5ab23b38223584210a0e959493afcc1f6214d068
mohitdudhatverselix <mohitdudhat22@gmail.com>
2024-09-24 15:49:14 +0530

    users model method added

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const { body, validationResult } = require('express-validator');
 const jwt = require('jsonwebtoken');
-const User = require('../models/User');
+const User = require('../models/user.model');
 
 const router = express.Router();
 

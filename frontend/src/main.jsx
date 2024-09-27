122677320fd56df6cae8bee847a238855ce938f5
mohitdudhatverselix <mohitdudhat22@gmail.com>
2024-09-28 00:18:44 +0530

    toast added

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,8 +5,9 @@ import './index.css'
 import { AuthProvider } from './contexts/AuthContext'
 import { BrowserRouter } from 'react-router-dom'
 import { GlobalProvider } from './contexts/GlobalContext'
-import { QueryClient, QueryClientProvider } from "react-query";
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { ReactQueryDevtools, QueryClientProvider } from '@tanstack/react-query-devtools';
+import { QueryClient } from '@tanstack/react-query'
+import { Toaster } from 'react-hot-toast';
 
 const queryClient = new QueryClient();
 createRoot(document.getElementById('root')).render(
@@ -17,9 +18,11 @@ createRoot(document.getElementById('root')).render(
     <AuthProvider>
       <App />
       <ReactQueryDevtools initialIsOpen={false} />
+      <Toaster position="top-center" reverseOrder={false} />
     </AuthProvider>
     </GlobalProvider>
       </BrowserRouter>
       </QueryClientProvider>
+
   </StrictMode>,
 )
